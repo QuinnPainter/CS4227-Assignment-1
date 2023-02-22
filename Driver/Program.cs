e0ba0f41cf36b2c92de835b0853c76ebbb93bc3e
Quinn Painter <quinn@geekymonkey.com>
2023-02-22 11:09:21 +0000

    Change project to SLN + add test module

---
+++ Driver/Program.cs
@@ -0,0 +1,16 @@
+﻿class Driver
+{
+    static void Main(string[] args)
+    {
+        var app = new Client.Application();
+        app.run();
+        
+        var movie1 = new Framework.Movie("Shrek", Framework.Movie.CHILDREN);
+        var movie2 = new Framework.Movie("Batman", Framework.Movie.NEW_RELEASE);
+        var cust1 = new Framework.Customer("Dave");
+        var cust2 = new Framework.Customer("Bob");
+        cust1.addRental(new Framework.Rental(movie1, 4));
+        cust2.addRental(new Framework.Rental(movie2, 1));
+        cust1.addRental(new Framework.Rental(movie2, 3));
+    }
+}
