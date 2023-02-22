cfea66c819ec3e34ac5336e62a4795df8269f648
Quinn Painter <quinn@geekymonkey.com>
2023-02-22 12:13:07 +0000

    Add test for adding a rental

--- Assignment/Framework/Customer.cs
+++ Assignment/Framework/Customer.cs
@@ -21,6 +21,11 @@ namespace Framework
             rentals.Add(rental);
         }
 
+        public List<Rental> getRentals()
+        {
+            return rentals;
+        }
+
         public String statement()
         {
             String result = "Rental Record for " + getName() + "\n";
--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -6,7 +6,12 @@ namespace Test;
 public class UnitTest1
 {
     [TestMethod]
-    public void TestMethod1()
+    public void TestCustomerAddRental()
     {
+        var cust = new Framework.Customer("TestCustomer");
+        var movie = new Framework.Movie("TestMovie", Framework.Movie.REGULAR);
+        var rental = new Framework.Rental(movie, 0);
+        cust.addRental(rental);
+        Assert.IsTrue(cust.getRentals().Contains(rental));
     }
 }
